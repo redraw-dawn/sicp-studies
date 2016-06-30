5395cffdbdfbba47cb8bce0440ed63241a655dea
Andrew Ward <apbward@gmail.com>
2016-06-30 21:25:57 +1000

    exercise 1.7 answered

--- exercises/one/exercises.scm
+++ exercises/one/exercises.scm
@@ -32,3 +32,31 @@
 ;; will be called indefinitely as only cond special form stops the evaluation at the predicate
 
 ;; exercise 1.7
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x) x)
+      ))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+;; start guessing square roots with 1
+(define (sqrt x)
+  (sqrt-iter 1.0 x))
+
+;; The test good-enough? is bad for very small numbers as it 0.001 may not be small enough a difference
+(sqrt 1.02000012312312)
+;; => 1.01000006156156
+(square 1.010000006156156)
+;; => 1.0201000124354351
+
+;; the test won't be adequate for large numbers because it is too minute a difference and will not return a value
+;; e.g.
+(sqrt 99991912481924812)
