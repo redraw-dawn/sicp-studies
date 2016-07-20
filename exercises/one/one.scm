226c435891f335d756fd05d6426855dda9318f5e
Andrew Ward <apbward@gmail.com>
2016-07-20 22:02:44 +1000

    finished exercise 1.9

--- exercises/one/one.scm
+++ exercises/one/one.scm
@@ -87,3 +87,82 @@
 
 (define (cbrt x)
   (cube-iter 1.0 0 x))
+
+;; Exercise 1.9
+;; Assumptions for how the following methods work. ignore + defined below
+(define (inc x) (+ x 1))
+(define (dec x) (- x 1))
+
+;; method 1
+(define (+ a b)
+  (if (= a 0)
+      b
+      (inc (+ (dec a) b))))
+
+;; method 1 working
+(+ 4 5)
+(if (= 4 0)
+    5
+    (inc (+ (dec 4) 5)))
+(inc (+ (- 4 1) 5))
+(inc (+ 3 5))
+(inc (if (= 3 0)
+	 5
+	 (inc (+ (dec 3) 5))))
+(inc (inc (+ (dec 3) 5)))
+(inc (inc (+ (- 3 1) 5)))
+(inc (inc (+ 2 5)))
+(inc (inc (if (= 2 0)
+	 5
+	 (inc (+ (dec 2) 5)))))
+(inc (inc (inc (+ (dec 2) 5))))
+(inc (inc (inc (+ (- 2 1) 5))))
+(inc (inc (inc (+ 1 5))))
+(inc (inc (inc (if (= 1 0)
+		   5
+		   (inc (+ (dec 1) 5))))))
+(inc (inc (inc (inc (+ (- 1 1) 5)))))
+(inc (inc (inc (inc (+ 0 5)))))
+(inc (inc (inc (inc (if (= 0 0)
+			5
+			(inc (+ (dec 0) 5)))))))
+
+(inc (inc (inc (inc 5))))
+(inc (inc (inc 6)))
+(inc (inc 7))
+(inc 8)
+9
+;; this process is recursive
+
+(define (+ a b)
+  (if (= a 0)
+      b
+      (+ (dec a) (inc b))))
+
+;; method 2 working
+(+ 4 5)
+(if (= 4 0)
+    5
+    (+ (dec 4) (inc 5)))
+(+ (dec 4) (inc 5))
+(+ 3 6)
+(if (= 3 0)
+    6
+    (+ (dec 3) (inc 6)))
+(+ (dec 3) (inc 6))
+(+ 2 7)
+(if (= 2 0)
+    7
+    (+ (dec 2) (inc 7)))
+(+ (dec 2) (inc 7))
+(+ 1 8)
+(if (= 1 0)
+    8
+    (+ (dec 1) (inc 8)))
+(+ (dec 1) (inc 8))
+(+ 0 9)
+(if (= 0 0)
+    9
+    (+ (dec 0) (inc 9)))
+9
+;; this process is iterative
