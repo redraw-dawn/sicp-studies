056222a6daf4444e154cf372e4771932f98c6a32
randbw <apbward@gmail.com>
2017-04-27 08:48:16 +1000

    Complete exercise 1.35

--- exercises/one/one.scm
+++ exercises/one/one.scm
@@ -569,3 +569,30 @@
 (f f)
 (f (lambda g) (g 2))
 ;; apply 2 to 2
+
+;; 1.35
+;; Golden ratio is a fixed point of the transformation x -> 1 + 1/x
+
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+(define (average list)
+  (define (inc n) (+ 1 n))
+  (define (loop list length to-divide)
+    (if (null? list)
+	(/ to-divide length)
+	(loop (cdr list) (inc length) (+ (car list) to-divide))))
+  (loop list 0 0.0))
+
+(define golden-ratio
+  (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
+
+
