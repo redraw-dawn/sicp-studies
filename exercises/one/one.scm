b0a52aedeca623c0d01d7a27fcf7cd630f468815
randbw <apbward@gmail.com>
2017-05-03 08:13:22 +1000

    Finished ex1.36

--- exercises/one/one.scm
+++ exercises/one/one.scm
@@ -595,4 +595,22 @@
 (define golden-ratio
   (fixed-point (lambda (x) (+ 1 (/ 1 x))) 1.0))
 
+;; 1.36
+;; Add logging
+(define tolerance 0.00001)
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (display next) (newline)
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
 
+(define without-damping
+  (fixed-point (lambda (x) (/ (log 1000) (log x))) 20.0))
+
+(define with-damping
+  (fixed-point (lambda (x) (/ (+ x (/ (log 1000) (log x))) 2)) 20.0))
