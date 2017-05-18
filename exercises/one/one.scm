5c9f57de176441fc0b0dd30e27dfdb0853f73bc2
randbw <apbward@gmail.com>
2017-05-19 08:38:50 +1000

    Finished 1.45

--- exercises/one/one.scm
+++ exercises/one/one.scm
@@ -735,3 +735,56 @@
 
 (define (n-fold-smooth f dx n)
     (repeated (smooth f dx) n))
+
+
+;; 1.45
+;; Make a procedure to find the n-th root using fixed-point, average-damp and repeated
+(define (average-damp f)
+  (lambda (x) (average x (f x))))
+
+(define (average a b)
+  (/ (+ a b) 2))
+
+(define (repeated f x)
+  (if (= x 1)
+      f
+      (compose f (repeated f (- x 1)))))
+
+(define (fixed-point f first-guess)
+  (define (close-enough? v1 v2)
+    (< (abs (- v1 v2)) tolerance))
+  (define (try guess)
+    (let ((next (f guess)))
+      (if (close-enough? guess next)
+          next
+          (try next))))
+  (try first-guess))
+
+;; expt is built in exponential function (which takes two args :/ )
+
+;; 1st test
+(define (nth-root n x)
+  (fixed-point
+   ((repeated average-damp 2)
+    (lambda (y) (/ x (expt y (- n 1)))))
+   1.0))
+
+;; 2nd test
+(define (nth-root n x)
+  (fixed-point
+   ((repeated average-damp 3)
+    (lambda (y) (/ x (expt y (- n 1)))))
+   1.0))
+
+
+;; final
+(define (safe-half n)
+  (if (= 0 (modulo n 2))
+      (/ n 2)
+      (/ (- n 1) 2)))
+
+(define (nth-root n x)
+  (fixed-point
+   ((repeated average-damp (safe-half n))
+    (lambda (y) (/ x (expt y (- n 1)))))
+   1.0))
