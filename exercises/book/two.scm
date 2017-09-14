851dc79e5daba000299f20e65dc9de171f5edd19
randbw <apbward@gmail.com>
2017-09-14 22:22:28 +1000

    Complete exercise 2.2

--- exercises/book/two.scm
+++ exercises/book/two.scm
@@ -9,3 +9,38 @@
 	 (denominator (correct-sign d))
 	 (g (gcd n d)))
     (cons (/ numerator g) (/ denominator g))))
+
+;; Exercise 2.2
+(define (make-segment start end)
+  (cons start end))
+
+(define (start-segment segment)
+  (car segment))
+
+(define (end-segment segment)
+  (cdr segment))
+
+(define (make-point x y)
+  (cons x y))
+
+(define (x-point point)
+  (car point))
+
+(define (y-point point)
+  (cdr point))
+
+(define (midpoint-segment segment)
+  (define (add-points segment getter)
+    (+ (getter (start-segment segment))
+       (getter (end-segment segment))))
+  (let ((x-avg (/ (add-points segment x-point) 2))
+	(y-avg (/ (add-points segment y-point) 2)))
+    (make-point x-avg y-avg)))
+
+(define (print-point p)
+  (newline)
+  (display "(")
+  (display (x-point p))
+  (display ",")
+  (display (y-point p))
+  (display ")"))
