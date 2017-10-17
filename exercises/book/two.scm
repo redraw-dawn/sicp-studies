fe48d9e37dc5f25e83da26265b48cd28a09fc6ef
randbw <apbward@gmail.com>
2017-10-18 08:42:28 +1100

    Finished ex2.6... finally

--- exercises/book/two.scm
+++ exercises/book/two.scm
@@ -134,3 +134,76 @@
 
 (define (cdr c)
   (reduce-pair c 3 0))
+
+;; Exercise 2.6
+
+(define zero
+  (lambda (f)
+    (lambda (x) x)))
+
+(define (zero f)
+  (lambda (x) x))
+
+(define (add-1 n)
+  (lambda (f)
+    (lambda (x)
+      (f ((n f) x)))))
+
+(add-1 zero)
+(add-1
+ (lambda (f)
+   (lambda (x) x)))
+
+(lambda (f)
+  (lambda (x)
+    (f (((lambda (f)
+	   (lambda (x) x)) f) x))))
+
+(lambda (f)
+  (lambda (x)
+    (f ((lambda (x) x) x))))
+
+(lambda (f)
+  (lambda (x)
+    (f x)))
+
+(define one
+  (lambda (f)
+    (lambda (x)
+      (f x))))
+
+(add-1 one)
+(add-1
+ (lambda (f)
+   (lambda (x)
+     (f x))))
+
+((lambda (n)
+   (lambda (f)
+     (lambda (x)
+       (f ((n f) x)))))
+ (lambda (f)
+   (lambda (x)
+     (f x))))
+
+(lambda (f)
+  (lambda (x)
+    (f (((lambda (f)
+	   (lambda (x)
+	     (f x)))
+	 f)
+	x))))
+
+(lambda (f)
+  (lambda (x)
+    (f ((lambda (x)
+	  (f x)) x))))
+
+(lambda (f)
+  (lambda (x)
+    (f (f x))))
+
+(define two
+  (lambda (f)
+    (lambda (x)
+      (f (f x)))))
