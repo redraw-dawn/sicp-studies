0d5990699a3d97be0a1443ff114ed3844b595fec
randbw <apbward@gmail.com>
2017-12-19 09:04:56 +1100

    Finished code part of 2.32

--- exercises/book/two.scm
+++ exercises/book/two.scm
@@ -795,3 +795,21 @@
 	     (tree-map fn subtree)
 	     (fn subtree)))
        tree))
+
+;; Exercise 2.32
+;; Complete the following definition of a procedure that generates the set of subsets
+;; of a set and give a clear explanation of why it works.
+;; must return => (() (3) (2) (2 3) (1) (1 3) (1 2) (1 2 3))
+(define nil '())
+
+(define (subsets s)
+  (if (null? s)
+      (list nil)
+      (let ((rest (subsets (cdr s))))
+        (append rest (map (lambda (x)
+			    (fringe (if (null? x)
+					(list (car s))
+					(cons (car s) (list x)))))
+			  rest)))))
+
+;; TODO: Explanation
