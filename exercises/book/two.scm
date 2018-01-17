1ab7b27b912ac7739dc55c46a60094af5719e3e7
randbw <apbward@gmail.com>
2018-01-18 08:27:02 +1100

    Answered 2.35

--- exercises/book/two.scm
+++ exercises/book/two.scm
@@ -848,3 +848,21 @@
 		(+ (* x higher-terms) this-coeff))
               0
               coefficient-sequence))
+
+;; Exercise 2.35
+;; Redefine count-leaves from section 2.2.2 as an accumulation:
+
+(define (count-leaves x)
+  (cond ((null? x) 0)
+        ((not (pair? x)) 1)
+        (else (+ (count-leaves (car x))
+                 (count-leaves (cdr x))))))
+
+(define (enumerate-tree tree)
+  (cond ((null? tree) nil)
+        ((not (pair? tree)) (list tree))
+        (else (append (enumerate-tree (car tree))
+                      (enumerate-tree (cdr tree))))))
+
+(define (count-leaves t)
+  (accumulate + 0 (map (lambda (x) 1) (enumerate-tree t))))
