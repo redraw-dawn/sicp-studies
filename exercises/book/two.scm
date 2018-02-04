4a2a8cc699e5e45a1c74c30f4def9246c3d929c8
randbw <apbward@gmail.com>
2018-02-05 09:04:16 +1100

    Answered 2.37

--- exercises/book/two.scm
+++ exercises/book/two.scm
@@ -880,3 +880,28 @@
       nil
       (cons (accumulate op init (map car seqs))
             (accumulate-n op init (map cdr seqs)))))
+
+;; Exercise 2.37
+(define (dot-product v w)
+  (accumulate + 0 (map * v w)))
+
+;; Fill in missing definitions
+(define (matrix-*-vector m v)
+  (map <??> m))
+
+(define (matrix-*-vector m v)
+  (map (lambda (r) (dot-product r v)) m))
+
+(define (transpose mat)
+  (accumulate-n <??> <??> mat))
+
+(define (transpose mat)
+  (accumulate-n cons '() mat))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map <??> m)))
+
+(define (matrix-*-matrix m n)
+  (let ((cols (transpose n)))
+    (map (lambda (r) (matrix-*-vector cols r)) m)))
