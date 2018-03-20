a655757eabb96e48d2c1ba2c2c1d1eae5cc5f0e5
randbw <apbward@gmail.com>
2018-03-21 08:37:32 +1100

    Answered 2.49

--- exercises/book/two.scm
+++ exercises/book/two.scm
@@ -1187,3 +1187,53 @@
 
 (define (end-segment segment)
   (cdr segment))
+
+;; Exercise 2.49
+;; Using segments->painter, answer the following:
+(define (segments->painter segment-list)
+  (lambda (frame)
+    (for-each
+     (lambda (segment)
+       (draw-line
+        ((frame-coord-map frame) (start-segment segment))
+        ((frame-coord-map frame) (end-segment segment))))
+     segment-list)))
+
+;; draw-line draws a line between two points
+
+(define (frame-coord-map frame)
+  (lambda (v)
+    (add-vect
+     (origin-frame frame)
+     (add-vect (scale-vect (xcor-vect v)
+                           (edge1-frame frame))
+               (scale-vect (ycor-vect v)
+                           (edge2-frame frame))))))
+
+;; a) a painter that draws the outline of the designated frame
+(define outline
+  (define outline-segments
+    (list
+     (make-segment (make-vect 0 0) (make-vect 0 1))
+     (make-segment (make-vect 0 1) (make-vect 1 1))
+     (make-segment (make-vect 1 1) (make-vect 1 0))
+     (make-segment (make-vect 1 0) (make-vect 0 0))))
+  (segments->painter outline-segments))
+;; b) a painter that draws an 'X' by connecting opposite corners of frame
+(define x
+  (define x-segments
+    (list
+     (make-segment (make-vect 0 0) (make-vect 1 1))
+     (make-segment (make-vect 0 1) (make-vect 1 0))))
+  (segments->painter x-segments))
+;; c) a painter that draws a diamond shape by connecting midpoints of sides frame
+(define diamond
+  (define diamond-segments
+    (list
+     (make-segment (make-vect 0 1) (make-vect 1 2))
+     (make-segment (make-vect 1 2) (make-vect 2 1))
+     (make-segment (make-vect 2 1) (make-vect 1 0))
+     (make-segment (make-vect 1 0) (make-vect 0 1))))
+  (segments->painter diamond-segments))
+;; d) the wave painter
+;; nup
